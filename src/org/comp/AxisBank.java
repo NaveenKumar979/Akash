d76a92ccc20d5716debed283710952362abe8e7c
NaveenKumar979 <naveenka660@gmail.com>
2023-08-16 14:44:08 +0530

    aksash

---
+++ src/org/comp/AxisBank.java
@@ -0,0 +1,10 @@
+package org.comp;
+
+public class AxisBank
+{
+	public void deposit()
+	{
+		System.out.println("xdeposit");
+
+}
+	}
---
+++ src/org/comp/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.comp;
+
+public class CompanyInfo extends AxisBank {
+private void companyName()
+{
+	System.out.println("max");
+}
+private void companyId()
+{
+System.out.println("id is 123");
+}
+private void companyAdress()
+{
+	System.out.println("2,kk nagar,vellore");
+}
+public static void main(String[] args)
+{
+	CompanyInfo b=new CompanyInfo();
+	b.companyName();
+	b.companyId();
+	b.companyAdress();
+}
+}
